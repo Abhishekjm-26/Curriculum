a96993ef437b2fcb6c6a07f9628ae5706d17327d
Abhishekjm-26 <61941623+Abhishekjm-26@users.noreply.github.com>
2022-04-26 18:55:54 +0530

    Update getPh.py

--- getPh.py
+++ getPh.py
@@ -2,13 +2,25 @@ def getPhoneNumber(s):
     l=["zero","one","two","three","four","five","six","seven","eight","nine"]
     st=""
     s=s.split()
-    for c in s:
-        st+=str(l.index(c))
+    i=0
+    while i<len(s):
+        j=i
+        if s[j]=="double":
+            st+=str(l.index(s[j+1]))
+            st+=str(l.index(s[j+1]))
+            i+=2
+        elif s[j]=="triple":
+            st+=str(l.index(s[j+1]))
+            st+=str(l.index(s[j+1]))
+            st+=str(l.index(s[j+1]))
+            i+=2
+        else:
+            st+=str(l.index(s[j]))
+            i+=1
+        
+        
+       
     return(st)
-
-if __name__ == '__main__':
-    s = str(input())
-    print(getPhoneNumber(s))
-   
-   
-   
+if _name=='main_':
+s=str(input(s)) 
+print(getPhoneNumber(s))
